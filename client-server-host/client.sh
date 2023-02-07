6c89ebae0b5b692a54a232158bf033c8ef5b445c
fchensan <fchensan@localhost>
2023-02-07 08:22:45 -0500

    Add remote vmstat execution

--- client-server-host/client.sh
+++ client-server-host/client.sh
@@ -1,7 +1,9 @@
 ulimit -n 100000
 if [[ $1 == nginx-host ]]
 then
-	sudo taskset -c 32-63 dpbench/bin/h1load -e -ll -P -t 32 -s 120 -d 120 -c 1024 -v http://10.10.1.3:80?K=1 | tee data/nginx-host.dat
+	ssh ubuntu@10.10.1.3 "rm /tmp/proxy-logs.txt; nohup vmstat 1 60 > /tmp/proxy-logs.txt 2> /dev/null < /dev/null &"
+	sudo taskset -c 32-63 dpbench/bin/h1load -e -ll -P -t 32 -s 60 -d 60 -c 1024 -v http://10.10.1.3:80 | tee data/nginx-host.dat
+	ssh ubuntu@10.10.1.3 "cat /tmp/proxy-logs.txt" | tee data-vmstat/nginx-host.dat
 fi
 
 
