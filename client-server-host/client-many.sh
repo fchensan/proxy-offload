f711a1ab294d501b0f08d395f6133f93b74f5edc
fchensan <fchensan@localhost>
2023-05-01 08:49:58 -0600

    Edit client script

---
+++ client-server-host/client-many.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+# ./client.sh tcp 10.10.1.4 fchensan direct-4096-conns-1mbps 32 300 1000000 ens1f0 ens1f1
+./client.sh tcp 10.10.1.4 fchensan direct-40000-conns-10kbps-no-reporting 400 700 10000 ens1f0 ens1f1
--- client-server-host/client.sh
+++ client-server-host/client.sh
@@ -13,23 +13,25 @@ then
 	fi
 elif [[ $1 == tcp ]]
 then
-	echo "Copying over config file"
+	# echo "Copying over config file"
 	scp -r ~/proxy-offload/proxy-host/ $PROXY_USERNAME@$PROXY_ADDR:~/proxy-offload
 
-	echo "Bootstrap NGINX files"
-	ssh $PROXY_USERNAME@$PROXY_ADDR "~/proxy-offload/proxy-host/nginx-conf/sites-available/generate-tcp-proxy.sh > ~/proxy-offload/proxy-host/nginx-conf/sites-available/tcp-proxy"
+	# echo "Bootstrap NGINX files"
+	# ssh $PROXY_USERNAME@$PROXY_ADDR "~/proxy-offload/proxy-host/nginx-conf/sites-available/generate-tcp-proxy.sh > ~/proxy-offload/proxy-host/nginx-conf/sites-available/tcp-proxy"
+	# ssh $PROXY_USERNAME@$PROXY_ADDR "~/proxy-offload/proxy-host/haproxy-conf/generate-haproxy-tcp.sh > ~/proxy-offload/proxy-host/haproxy-conf/haproxy-tcp.cfg"
 
 	ssh $PROXY_USERNAME@$PROXY_ADDR "sudo pkill monitor.sh"
-	ssh $PROXY_USERNAME@$PROXY_ADDR "sudo pkill nginx"
+	# ssh $PROXY_USERNAME@$PROXY_ADDR "sudo pkill haproxy"
+	# ssh $PROXY_USERNAME@$PROXY_ADDR "sudo pkill nginx"
 
-	echo "Restarting NGINX in proxy host"
-	ssh $PROXY_USERNAME@$PROXY_ADDR "sudo rm /var/log/nginx/error.log"
-	ssh $PROXY_USERNAME@$PROXY_ADDR "~/proxy-offload/proxy-host/proxy-setup.sh tcp nginx ~/proxy-offload/proxy-host/nginx-conf"
+	# echo "Restarting NGINX in proxy host"
+	# ssh $PROXY_USERNAME@$PROXY_ADDR "sudo rm /var/log/nginx/error.log"
+	# ssh $PROXY_USERNAME@$PROXY_ADDR "~/proxy-offload/proxy-host/proxy-setup.sh tcp haproxy ~/proxy-offload/proxy-host/haproxy-conf"
 
 	PORT_ONE=$8
 	PORT_TWO=$9
 
-	echo "Starting monitor.sh in proxy host"
+	# echo "Starting monitor.sh in proxy host"
 	ssh $PROXY_USERNAME@$PROXY_ADDR "~/proxy-offload/proxy-host/monitor-wrapper.sh $PORT_ONE $PORT_TWO 20"
 
 	sleep 3
@@ -38,11 +40,11 @@ then
 	DURATION=$6
 	CLIENT_PORT_START=30000
 	SERVER_PORT_END=$((29999+PROCESSES))
-	STREAMS_PER_PROCESS=128
+	STREAMS_PER_PROCESS=100
 	TARGET_BITRATE=$7
 	SPAWN_NEW_PROCESS_RATE=1
 
-	cd /mydata
+	# cd /mydata
 	mkdir -p $EXPERIMENT_TITLE
 	cd $EXPERIMENT_TITLE
 	mkdir -p logs-$PROCESSES
@@ -56,24 +58,40 @@ then
 	echo "Duration: $DURATION" | tee -a README.txt
 	echo "Streams per process: $STREAMS_PER_PROCESS" | tee -a README.txt
 	echo "Target bitrate: $TARGET_BITRATE" | tee -a README.txt
-	echo "Sleep between spawning new process: $SPAWN_NEW_PROCESS_RATE sec" | tee -a README.txt 
+	echo "Sleep between spawning new process: $SPAWN_NEW_PROCESS_RATE sec" | tee -a README.txt
+
+	nohup ~/proxy-offload/proxy-host/monitor.sh $PORT_ONE $PORT_TWO 20 > monitor-client.log 2> /dev/null &
+	nohup sar 20 > sar-client.log 2> /dev/null &
 	
+	START_TIME=$(date +%s)
+
 	for PORT in $(seq $CLIENT_PORT_START $SERVER_PORT_END) 
 	do
 		echo -ne "$PORT/$SERVER_PORT_END                         \r\c"
-		nohup ~/iperf/src/iperf3 -c $PROXY_ADDR -f m -p $PORT -P $STREAMS_PER_PROCESS -b $TARGET_BITRATE -i 60 -t $DURATION --timestamp > logs-$PROCESSES/iperf3-$PORT.json 2> logs-$PROCESSES/iperf3-$PORT.err &
+		CURR_TIME=$(date +%s)
+		ELAPSED_TIME=$(( CURR_TIME-START_TIME ))
+		nohup ~/iperf/src/iperf3 -c $PROXY_ADDR -f m -p $PORT -P $STREAMS_PER_PROCESS -b $TARGET_BITRATE -i 0 -t $(( DURATION - ELAPSED_TIME )) > logs-$PROCESSES/iperf3-$PORT.json 2> logs-$PROCESSES/iperf3-$PORT.err &
 		sleep $SPAWN_NEW_PROCESS_RATE
+		# if [ $PORT -ne $CLIENT_PORT_START ] && [ $((PORT % 200)) -eq 0 ]; then
+		# 	sleep 300
+		# fi
 	done
 	echo "$SERVER_PORT_END/$SERVER_PORT_END [Done]"
 
-	echo "Letting experiment run for another $DURATION second(s). It's currently $(date)"
-	sleep $DURATION 
+	sleep 15
+	echo "Letting experiment run for another $(( DURATION - ELAPSED_TIME + 10)) second(s). It's currently $(date)"
+	sleep $(( DURATION - ELAPSED_TIME + 1210)) 
 	echo "Done!"
 
-	echo "Stopping monitor.sh and retrieving data"
+	# echo "Stopping monitor.sh and retrieving data"
 	ssh $PROXY_USERNAME@$PROXY_ADDR "sudo pkill monitor.sh"
-	sleep 2
-	ssh $PROXY_USERNAME@$PROXY_ADDR "cat ~/proxy-offload/proxy-host/data.log" > monitor.log
-	ssh $PROXY_USERNAME@$PROXY_ADDR "sudo cat /var/log/nginx/error.log" > nginx-error.log
-	ssh $PROXY_USERNAME@$PROXY_ADDR "sudo nginx -s quit"
+	ssh $PROXY_USERNAME@$PROXY_ADDR "sudo pkill sar"
+	# sleep 2
+	ssh $PROXY_USERNAME@$PROXY_ADDR "cat ~/proxy-offload/proxy-host/data.log" > monitor-server.log
+	ssh $PROXY_USERNAME@$PROXY_ADDR "cat ~/proxy-offload/proxy-host/sar-server.log" > sar-server.log
+	# ssh $PROXY_USERNAME@$PROXY_ADDR "sudo cat /var/log/nginx/error.log" > nginx-error.log
+	# ssh $PROXY_USERNAME@$PROXY_ADDR "sudo pkill haproxy"
+
+	sudo pkill monitor
+	sudo pkill sar
 fi
