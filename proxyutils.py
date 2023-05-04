8be75bc07420f19c37aba1c87706247b43acbd65
fchensan <fchensan@localhost>
2023-05-04 13:14:08 -0400

    Fix

--- proxyutils.py
+++ proxyutils.py
@@ -10,6 +10,8 @@ START_SAR = 1
 STOP_RETRIEVE_SAR = 2
 RETRIEVE_IPERF = 3
 RESET = 4
+START_MONITOR = 5
+STOP_RETRIEVE_MONITOR = 6
 
 def is_json(string):
     try:
@@ -70,8 +72,10 @@ class Node():
     def start_sar(self):
         self.send_command(START_SAR)
 
-    def start_monitor_script(self):
-        pass
+    def start_monitor_script(self, interface):
+        self.send_command(START_MONITOR, {
+            "interface": interface
+        })
     
     def retrieve_iperf_log(self, port, filepath):
         received_data = self.send_command(RETRIEVE_IPERF, options={"port":port}, receive=True)
@@ -85,8 +89,10 @@ class Node():
         with open(filepath, "w") as file:
             file.write(received_data['content'])
 
-    def stop_and_retrieve_monitor_script(self):
-        pass
+    def stop_and_retrieve_monitor_script(self, filepath):
+        received_data = self.send_command(STOP_RETRIEVE_MONITOR, receive=True)
+        with open(filepath, "w") as file:
+            file.write(received_data['content'])
 
     def reset(self):
         self.send_command(RESET)
@@ -115,8 +121,9 @@ class Agent():
         command = f"nohup sar 20 > /tmp/temp-sar.log 2> /dev/null &"
         subprocess.run(command, shell=True)
 
-    def start_monitor_script(self):
-        pass
+    def start_monitor_script(self, interface):
+        command = f"nohup ~/proxy-offload/proxy-host/monitor.sh 20 {interface} > /tmp/temp-monitor.log 2> /dev/null &"
+        subprocess.run(command, shell=True)
 
     def stop_and_retrieve_sar(self, connection):
         subprocess.run("sudo pkill sar".split())
@@ -137,7 +144,13 @@ class Agent():
         connection.sendall(json_data.encode("utf-8"))
             
     def stop_and_retrieve_monitor_script(self):
-        pass
+        command = f"sudo pkill monitor.sh;"
+        subprocess.run(command, shell=True)
+        with open("/tmp/temp-monitor.log", 'r') as file:
+            file_contents = file.read()
+            data = {"content": file_contents}
+            json_data = json.dumps(data)
+            connection.sendall(json_data.encode("utf-8"))
 
     def reset(self):
         command = f"sudo pkill iperf; sudo pkill sar;"
@@ -186,6 +199,9 @@ class Agent():
                     self.retrieve_iperf_log(connection, options['port'])
                 elif received_data['command'] == RESET:
                     self.reset()
+                elif received_data['command'] == START_MONITOR:
+                    options = received_data['options']
+                    self.start_monitor_script(options['interface'])
 
                 # Close the connection
                 connection.close()
