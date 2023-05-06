9a75cee7c3f67ff6407cc13ac276dd1e44eb5041
Francisco Chensan <chensanfrancisco@gmail.com>
2023-05-06 09:12:18 +0800

    Update script

--- setup.py
+++ setup.py
@@ -2,3 +2,10 @@ cd
 sudo apt-get update
 sudo apt-get install haproxy sysstat
 sudo systemctl stop haproxy
+git clone https://github.com/esnet/iperf.git
+cd iperf
+./configure
+sudo make
+sudo make install
+cd
+echo 1024 65535 | sudo tee /proc/sys/net/ipv4/ip_local_port_range
